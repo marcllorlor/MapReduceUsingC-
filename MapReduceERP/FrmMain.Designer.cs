876b1009667783b4140d9a297e84bf58770822b8
Marc Llorca <marcllorlor@gmail.com>
2023-06-01 11:34:42 +0200

    Deploy

---
+++ MapReduceERP/FrmMain.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MapReduceERP
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnObrirArxiu1 = new System.Windows.Forms.Button();
+            this.btnObrirArxiu2 = new System.Windows.Forms.Button();
+            this.btnObrirArxiu3 = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.tbArxiu1 = new System.Windows.Forms.TextBox();
+            this.tbArxiu2 = new System.Windows.Forms.TextBox();
+            this.tbArxiu3 = new System.Windows.Forms.TextBox();
+            this.lbTitol = new System.Windows.Forms.Label();
+            this.lbLlistaCompletaParaules = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnObrirArxiu1
+            // 
+            this.btnObrirArxiu1.Location = new System.Drawing.Point(246, 58);
+            this.btnObrirArxiu1.Name = "btnObrirArxiu1";
+            this.btnObrirArxiu1.Size = new System.Drawing.Size(159, 23);
+            this.btnObrirArxiu1.TabIndex = 0;
+            this.btnObrirArxiu1.Text = "Buscar Arxiu 1";
+            this.btnObrirArxiu1.UseVisualStyleBackColor = true;
+            this.btnObrirArxiu1.Click += new System.EventHandler(this.btnObrirArxiu1_Click);
+            // 
+            // btnObrirArxiu2
+            // 
+            this.btnObrirArxiu2.Location = new System.Drawing.Point(246, 161);
+            this.btnObrirArxiu2.Name = "btnObrirArxiu2";
+            this.btnObrirArxiu2.Size = new System.Drawing.Size(159, 23);
+            this.btnObrirArxiu2.TabIndex = 1;
+            this.btnObrirArxiu2.Text = "Buscar Arxiu 2";
+            this.btnObrirArxiu2.UseVisualStyleBackColor = true;
+            this.btnObrirArxiu2.Click += new System.EventHandler(this.btnObrirArxiu2_Click);
+            // 
+            // btnObrirArxiu3
+            // 
+            this.btnObrirArxiu3.Location = new System.Drawing.Point(246, 264);
+            this.btnObrirArxiu3.Name = "btnObrirArxiu3";
+            this.btnObrirArxiu3.Size = new System.Drawing.Size(159, 23);
+            this.btnObrirArxiu3.TabIndex = 2;
+            this.btnObrirArxiu3.Text = "Buscar Arxiu 3";
+            this.btnObrirArxiu3.UseVisualStyleBackColor = true;
+            this.btnObrirArxiu3.Click += new System.EventHandler(this.btnObrirArxiu3_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(421, 150);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(99, 45);
+            this.btnExportar.TabIndex = 3;
+            this.btnExportar.Text = "Reduce";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // tbArxiu1
+            // 
+            this.tbArxiu1.Location = new System.Drawing.Point(41, 58);
+            this.tbArxiu1.Name = "tbArxiu1";
+            this.tbArxiu1.Size = new System.Drawing.Size(185, 22);
+            this.tbArxiu1.TabIndex = 4;
+            // 
+            // tbArxiu2
+            // 
+            this.tbArxiu2.Location = new System.Drawing.Point(41, 162);
+            this.tbArxiu2.Name = "tbArxiu2";
+            this.tbArxiu2.Size = new System.Drawing.Size(185, 22);
+            this.tbArxiu2.TabIndex = 5;
+            // 
+            // tbArxiu3
+            // 
+            this.tbArxiu3.Location = new System.Drawing.Point(41, 264);
+            this.tbArxiu3.Name = "tbArxiu3";
+            this.tbArxiu3.Size = new System.Drawing.Size(185, 22);
+            this.tbArxiu3.TabIndex = 6;
+            // 
+            // lbTitol
+            // 
+            this.lbTitol.AutoSize = true;
+            this.lbTitol.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitol.Location = new System.Drawing.Point(203, 9);
+            this.lbTitol.Name = "lbTitol";
+            this.lbTitol.Size = new System.Drawing.Size(246, 39);
+            this.lbTitol.TabIndex = 7;
+            this.lbTitol.Text = "MAP REDUCE";
+            // 
+            // lbLlistaCompletaParaules
+            // 
+            this.lbLlistaCompletaParaules.FormattingEnabled = true;
+            this.lbLlistaCompletaParaules.ItemHeight = 16;
+            this.lbLlistaCompletaParaules.Location = new System.Drawing.Point(526, 12);
+            this.lbLlistaCompletaParaules.Name = "lbLlistaCompletaParaules";
+            this.lbLlistaCompletaParaules.Size = new System.Drawing.Size(229, 308);
+            this.lbLlistaCompletaParaules.TabIndex = 8;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 337);
+            this.Controls.Add(this.lbLlistaCompletaParaules);
+            this.Controls.Add(this.lbTitol);
+            this.Controls.Add(this.tbArxiu3);
+            this.Controls.Add(this.tbArxiu2);
+            this.Controls.Add(this.tbArxiu1);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnObrirArxiu3);
+            this.Controls.Add(this.btnObrirArxiu2);
+            this.Controls.Add(this.btnObrirArxiu1);
+            this.Name = "FrmMain";
+            this.Text = "Map Reduce Marc Llorca";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnObrirArxiu1;
+        private System.Windows.Forms.Button btnObrirArxiu2;
+        private System.Windows.Forms.Button btnObrirArxiu3;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.TextBox tbArxiu1;
+        private System.Windows.Forms.TextBox tbArxiu2;
+        private System.Windows.Forms.TextBox tbArxiu3;
+        private System.Windows.Forms.Label lbTitol;
+        private System.Windows.Forms.ListBox lbLlistaCompletaParaules;
+    }
+}
+
---
+++ MapReduceERP/FrmMain.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapReduceERP
+{
+    public partial class FrmMain : Form
+    {
+        String nomarxiu1, nomarxiu2, nomarxiu3;
+
+        String contingutarxiu1 = String.Empty,contingutarxiu2 = String.Empty, contingutarxiu3 = String.Empty;
+
+        Hashtable taulaHash;
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+
+
+
+            //Aqui farem  un foreach per cada element que hi hagi a dins d'una string, abans haurem de polir les strings treient els salts de linia
+
+            corregirStrings();
+
+            afegirdadestaulaHash();
+
+            mostrardadeslistbox();
+        }
+
+        private void mostrardadeslistbox()
+        {
+            foreach(char key in taulaHash.Keys)
+            {
+                String dadacompleta = key + "," + taulaHash[key].ToString();
+                lbLlistaCompletaParaules.Items.Add(dadacompleta);
+            }
+        }
+
+        private void afegirdadestaulaHash()
+        {
+            taulaHash = new Hashtable();
+
+            foreach (char caracter in contingutarxiu1)
+            {
+                if (!taulaHash.ContainsKey(caracter))
+                {
+                    taulaHash.Add(caracter, 1);
+                }
+                else
+                {
+                    int valor = (int)taulaHash[caracter];
+                    taulaHash[caracter] = valor + 1;
+                }
+            }
+
+            foreach (char caracter in contingutarxiu2)
+            {
+                if (!taulaHash.ContainsKey(caracter))
+                {
+                    taulaHash.Add(caracter, 1);
+                }
+                else
+                {
+                    int valor = (int)taulaHash[caracter];
+                    taulaHash[caracter] = valor + 1;
+                }
+            }
+
+            foreach (char caracter in contingutarxiu2)
+            {
+                if (!taulaHash.ContainsKey(caracter))
+                {
+                    taulaHash.Add(caracter, 1);
+                }
+                else
+                {
+                    int valor = (int)taulaHash[caracter];
+                    taulaHash[caracter] = valor + 1;
+                }
+            }
+        }
+
+        private void corregirStrings()
+        {
+            contingutarxiu1 = contingutarxiu1.Trim().ToUpper().Replace(" ", "").Replace(",", "").Replace(".", "");
+            contingutarxiu2 = contingutarxiu2.Trim().ToUpper().Replace(" ", "").Replace(",", "").Replace(".", "");
+            contingutarxiu3 = contingutarxiu3.Trim().ToUpper().Replace(" ", "").Replace(",", "").Replace(".", "");
+        }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObrirArxiu1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                nomarxiu1 = openFileDialog.FileName;
+                tbArxiu1.Text = nomarxiu1;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    contingutarxiu1 = reader.ReadToEnd();
+                }
+            }
+        }
+
+        private void btnObrirArxiu2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                nomarxiu2 = openFileDialog.FileName;
+                tbArxiu2.Text = nomarxiu2;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    contingutarxiu2 = reader.ReadToEnd();
+                }
+            }
+        }
+
+        private void btnObrirArxiu3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                nomarxiu3 = openFileDialog.FileName;
+                tbArxiu3.Text = nomarxiu3;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    contingutarxiu3 = reader.ReadToEnd();
+                }
+            }
+        }
+
+
+
+    }
+}
